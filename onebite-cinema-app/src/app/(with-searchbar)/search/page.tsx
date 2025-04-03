f0d956f77dd6af9a73afb57dedaa9f34de9b9be6
woo-dev-log <sswwhh.dev@gmail.com>
2025-04-03 15:50:32 +0900

    day9 App router 페이지, 레이아웃 설정

---
+++ onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page({
+  searchParams
+}: {
+  searchParams: Promise<{ q: string }>
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <div>Search : {q}</div>
+  );
+}
